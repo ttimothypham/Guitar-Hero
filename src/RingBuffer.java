916b1b7e5d049c695b379ba190ef24c41965f68c
ttimothypham <pham.t.timothy@gmail.com>
2023-11-09 10:33:51 -0600

    isEmpty() done

--- src/RingBuffer.java
+++ src/RingBuffer.java
@@ -2,12 +2,12 @@
 public class RingBuffer {
 	
 	private int first = 0;
-	private int last;
-	private int[] values;
+	private int last = 0;
+	private double[] values;
 
 	public RingBuffer(int capacity)
 	{
-		values = new int[capacity];
+		values = new double[capacity];
 	}
 	
 	public int size()
@@ -27,7 +27,26 @@ public class RingBuffer {
 	
 	public void enqueue(double x)
 	{
-		
+		if (!isFull()) //checks if the array is not full
+		{
+			values[last] = x; //enqueue x to last
+			if (last + 1 == values.length) //checks if last needs to be wrapped
+			{
+				last = 0; //wraps last to index 0
+			}
+			else //no wrap is needed
+			{
+				last++; //increments last
+			}
+		}
+		if (values.size() == 0) //checks if the array is empty
+		{
+			values[first] = x; //sets x in the first index
+		}
+		if (isFull())
+		{
+			throw new IllegalStateException("Buffer is full");
+		}
 	}
 	
 	public double dequeue()
