7b50c53f0242a3d431a708057bc1784ad1a14bec
Destroyer3po <94648238+Destroyer3po@users.noreply.github.com>
2023-11-09 10:34:20 -0600

    ringBuffer
    did some of the methods

--- src/RingBuffer.java
+++ src/RingBuffer.java
@@ -2,17 +2,21 @@
 public class RingBuffer {
 	
 	private int first = 0;
-	private int last;
-	private int[] values;
+	private int last = 0;
+	private double[] values;
 
 	public RingBuffer(int capacity)
 	{
-		values = new int[capacity];
+		values = new double[capacity];
 	}
 	
 	public int size()
 	{
-		
+		if(first < last)
+		{
+			return last - first; 
+		}
+		return values.length - first + last;
 	}
 	
 	public boolean isEmpty()
@@ -32,16 +36,34 @@ public class RingBuffer {
 	
 	public double dequeue()
 	{
+		double returning = values[first]; 
+		values[first] = 0; 
+		first ++; 
+		if(first >= values.length)
+		{
+			first = 0; 
+		}
+		return returning; 
 		
 	}
 	
 	public double peek()
 	{
+		return values[first];
 		
 	}
 	
 	public String toString()
 	{
 		
+		String returning = ""; 
+		int start = first; 
+		while(start != last)
+		{
+			returning += values[start]; 
+			start = (start + 1) % values.length; 
+
+		}
+		return returning; 
 	}
 }
